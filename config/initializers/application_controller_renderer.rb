ccd6e3fb91aae9b970f45b227a7c398a42d9c1ee
川上鈴乃 <kawakamisuzuno@kawakamisuzunonoMacBook-Pro.local>
2020-02-18 19:40:59 +0900

    [finish]5-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8ae30dc5c281acfd428f4ad84056363feb5f07523197756ddaf336de48624680d3887d2183b8c9bad68cfb3c18b47999316e308a0b5dddf69d3e0ee71369eff9'
